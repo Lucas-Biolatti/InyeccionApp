19d83435e11f96983727f2fcf56f0d434f02a9c4
Lucas-Biolatti <lucasbiolatti@gmail.com>
2023-10-08 12:34:27 -0300

    081023

--- public/javascripts/dashboard.js
+++
@@ -1,6 +0,0 @@
-
-fetch(`moldesEntreFecha?fini=${fini}&ffin=${ffin}&molde=${molde}`)
-         .then(response => response.json())
-         .then(data =>{
-            
-         })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var conexion = require('../database/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/moldesEntreFecha',(req,res)=>{
+  let fini=req.query.fini;
+  let ffin=req.query.ffin;
+  let molde=req.query.molde;
+
+  let sql = `CALL moldeEntreFecha('${fini}','${ffin}','${molde}')`
+  conexion.query(sql,(err,result)=>{
+    if (!err) {
+      res.send(result[0]);
+    }else{
+      console.log(err);
+    }
+  })
+
+})
 module.exports = router;
